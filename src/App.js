7f6028ef0b1b8c690aeb22650f7f1f2994dedea7
unknown <iam.mhd07@gmail.com>
2019-10-07 17:13:29 +0700

    add 2.add_todo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 
-import Todo from './components/ToDo';
-//import Todo from './components/AddTodo';
+// import Todo from './components/ToDo';
+import Todo from './components/AddTodo';
 //import Todo from './components/DeleteTodo';
 // import Todo from './components/DoneTodo';
 //import Todo from './components/UpdateTodo';
---
+++ src/components/AddTodo.js
@@ -0,0 +1,120 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+
+export default class AddTodo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      getTodo: '',
+      todo: ['work', 'swim', 'study', 'sleep', 'running']
+    };
+  }
+
+  addTodo() {
+    const { getTodo } = this.state;
+    if (this.state.getTodo === '') {
+      alert('Field Todo Belum Diisi');
+    } else {
+      this.setState({
+        todo: [...this.state.todo, getTodo],
+        getTodo: ''
+      });
+    }
+  }
+
+  deleteTodo() {
+
+  }
+
+  render() {
+    //console.log(this.state.newTodo);
+    return (
+      <View>
+
+        <View style={styles.containInput}>
+          <View style={styles.input}>
+            <TextInput
+              placeholder='New todo'
+              onChangeText={text => this.setState({ getTodo: text })}
+              value={this.state.getTodo}
+            />
+          </View>
+
+          <View>
+            <TouchableOpacity
+              style={styles.buttonStyle}
+              onPress={() => this.addTodo()}
+            >
+              <Text style={styles.buttonText}>
+                Add
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View>
+          {this.state.todo.map(item => {
+            return (
+              <View
+                key={item}
+                style={styles.list}
+              >
+                <Text style={styles.textList}>{item}</Text>
+              </View>
+            );
+          })}
+        </View>
+
+      </View>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  containInput: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    marginBottom: 30,
+    marginHorizontal: 20,
+    marginTop: 20,
+    alignItems: 'center'
+  },
+  input: {
+    borderWidth: 2,
+    paddingLeft: 5,
+    borderColor: 'silver',
+    borderRadius: 2,
+    elevation: 1,
+    width: '70%',
+    height: 45,
+  },
+  list: {
+    flexDirection: 'row',
+    paddingBottom: 5,
+    marginBottom: 10,
+    borderBottomColor: '#000',
+    borderBottomWidth: 2,
+    paddingLeft: 15,
+
+    justifyContent: 'space-between',
+    paddingRight: 20
+  },
+  textList: {
+    fontSize: 20,
+    textTransform: 'capitalize',
+  },
+  buttonStyle: {
+    marginLeft: 5,
+    backgroundColor: '#ddd',
+    paddingVertical: 10,
+    paddingHorizontal: 24,
+    borderWidth: 2,
+    borderColor: 'silver',
+    borderRadius: 2,
+    elevation: 2,
+  },
+  buttonText: {
+    fontWeight: 'bold'
+  }
+});
